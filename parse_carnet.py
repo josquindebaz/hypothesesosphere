8ef6ee3025a4b4a9225640948ca171bb1e4791cd
josquindebaz <josquin.debaz@gmail.com>
2018-10-19 16:36:53 +0200

    modifié :         parse_carnet.py

--- parse_carnet.py
+++ parse_carnet.py
@@ -1,72 +1,88 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
 import urllib.request
+from bs4 import BeautifulSoup
 import re
 import json
 
-url = "https://socioargu.hypotheses.org/5306"
-
-class parsepage(object):
+class get_content(object):
     def __init__(self, url):
-        self.getcarnet(url)
         print (url, "recupere")
-        post = self.recupmeta() 
-        if (post):
-            print (post)
-            #md_json = json.loads(post)
-            #print(md_json)
-
-        """TODO decoupe, recupere tous les urls
-        separe carnets, images et autres liens
-        genere log json avec toutes ces infos si c'est un post et pas un autre type d'entree
-        ajoute non presentes a la liste des pages a parser"""
-
-    def getcarnet(self, url):
         with urllib.request.urlopen(url) as response:
             html = response.read()
             self.html = html.decode('utf-8')
 
-    def recupmeta(self, html=None):
-        """TODO recupérer taille, date """
-        if not (html): 
-            html = self.html
+
+
+class parsepage(object):
+    def __init__(self, html):
+        soup = BeautifulSoup(html, "lxml")
+
+        #Get all links to hypotheses.org
+        to_follow = filter( self.is_carnet, self.recup_urls(soup) )   
+
+        post = self.recupmeta(html) 
+        if (post):
+            #Take the content of the post
+            content = (soup.article)
+            #Get images
+            post['imgs'] = self.recup_img(content)
+            #Get urls
+            post['urls'] = self.recup_urls(content)
+            #json for log
+            js = json.dumps(post)
+
+            """
+            #Get carnets from urls
+            self.recup_names_carnets (filter(self.is_carnet, urls))
+            """
+           
+
+        """TODO 
+        separe carnets, images et autres liens
+        genere log json avec toutes ces infos si c'est un post et pas un autre type d'entree
+        ajoute non presentes a la liste des pages a parser"""
+
+    def recupmeta(self, html):
+        """ If it's a post, get date and size, Else return False """
         if re.search('time class="entry-date" datetime=', html):
             md = {'date' : re.findall('time class="entry-date" datetime="([\d-]*)T\S*">', html)[0] } 
             md['size'] = len(html)
             return md 
         else:
             print ("not a post")
-            return 0
+            return False 
 
-    def decoupe_main(self, html=None):
-        if not (html): 
-            html = self.html
-        main = re.split('<div id="main" class="wrapper">', html)[1]
-        self.main = re.split('</div><!-- #main .wrapper -->', main)[0]
-        #networks_button = re.split('<ul class="soc">', without_footer)
-        #without_footer = networks_button[0] + re.split("</ul>", networks_button[1], 1)[1]
+    def is_carnet(self, url): 
+        return re.search("^\w*.hypotheses.org", url) 
 
-    def recup_urls(self, html=None):
-        if not (html): 
-            html = self.main
-
-        urls = re.findall('https?://(\S*)["\']', html) 
-
-        urls = map(lambda x: re.sub('/$', '', x), urls)
-        urls = map(lambda x: re.split('#', x)[0], urls)
-        self.urls = list (set (urls))
-        print ( len(self.urls) )
-
-    def recup_urls_carnet(self, urls=None):
-        """TODO separe tous les liens qui sont des carnets des autres"""
-        if not (urls): 
-            urls = self.urls
-        for i in urls:
-            if re.search("^\w*.hypotheses.org", i): 
-                print (i)
+    def recup_names_carnets(self, urls):
+        """ Get Carnet Names from url"""
+        L = map( lambda x: re.search("^(\w*).hypotheses.org", x).group(1), urls)
+        return list(set(L))
     
-    def recup_img(self, urls=None):
-        """TODO recup images du post"""
-        pass
+    def recup_img(self, soup):
+        """Get images with hard src"""
+        list_img = []
+        for img in soup.find_all('img'):
+            list_img.extend(re.findall("http\S*", str(img)))
+        return list(set(list_img))
+
+    def recup_urls(self, soup):
+        """Get urls fom <a """
+        list_url = []
+        for url in soup.find_all('a', href=True):
+            href = url['href']
+            href = re.sub("https?://(www.)?", "", href) 
+            href = re.sub("(#.*|/$)", "", href)
+            list_url.append(href)
+        return list(set(list_url))
 
 
-
-parsepage(url)
+if __name__ == '__main__' :
+    #url = "https://socioargu.hypotheses.org/5306"
+    with open('test.html', 'r') as F:
+        B = F.read()
+        parsepage(B)
